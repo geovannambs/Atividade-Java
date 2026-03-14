5514372ae885ec9952f3bfb6fbb2fe69e6e19336
geovannambs <geovannambsantos@gmail.com>
2026-03-14 10:05:25 -0300

    Questão 1.js

--- Atividade Java/Questão 1.js
+++ Atividade Java/Questão 1.js
@@ -1,9 +1,9 @@
 var numero = Math.floor(Math.random() * 20) + 1
 var x = 0
 
-while ( x != numero) {
+while (x != numero) {
 
-chute = prompt("Advinhe o número de 1 a 20")
+    x = Number(prompt("Advinhe o número de 1 a 20"))
 
     if (x > numero) {
         console.log("O número é menor")
